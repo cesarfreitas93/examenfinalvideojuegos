da159a58b20050126be4831b45e60de76a1005d0
Cesar Freitas <33384671+cesarfreitas93@users.noreply.github.com>
2022-03-08 14:23:33 -0500

    First Commit

---
+++ js/dark.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector('#toggle');
+const body = document.body;
+/*
+toggle.addEventListener('click', () => {
+   if (body.classList.contains('dark')) {
+      body.classList.remove('dark');
+   } else {
+      body.classList.add('dark');
+   }
+});
+*/
+body.classList.remove('dark')
